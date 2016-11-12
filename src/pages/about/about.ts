a6f4804ce69fb8ec81e54fc5b50262c1417f9029
Paulomi Mahidharia <mahidharia.p@husky.neu.edu>
2016-11-12 14:30:49 -0500

    Edit and delete(fully functional) for each item working.

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -10,13 +10,44 @@ export class AboutPage {
 
   items = [];
   deleteList(event){};
+  editList(event){};
+  showEditIcons = false;
+  removeItem(item){};
+  editItem(item){};
 
   constructor(public navCtrl: NavController) {
 
-    this.items = ['Paul', 'Monzy', 'Nick', 'Jay'];
+    this.showEditIcons = false;
+
+    this.items = [{"id": "1", "name":'Paul'},
+      {"id": "2", "name":'Nick'},
+      {"id": "3", "name":'Monzy'},
+      {"id": "4", "name":'Jay'}];
     this.deleteList = function (event) {
       this.items = [];
     }
+    this.editList = function (event) {
+      console.log(this.showEditIcons);
+      if(this.showEditIcons == false){
+        this.showEditIcons = true;
+      }else{
+        this.showEditIcons = false;
+      }
+    };
+
+    this.removeItem = function (item) {
+
+      var index = this.items.indexOf(item);
+      if(index == -1){
+        return;
+      }
+      this.items.splice(index, 1);
+    };
+
+    this.editItem = function(item){
+      //Wait for monzy
+    }
+
   }
 
 }
