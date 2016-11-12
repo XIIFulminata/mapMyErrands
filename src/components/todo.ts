2a76ce36da4a18bfddf82f135b68fa14d57b4b0c
NICHOLAS A FEALEY <nicholas.fealey@ahold.com>
2016-11-12 16:24:03 -0500

    add todo object and service for storing todos

---
+++ src/components/todo.ts
@@ -0,0 +1,6 @@
+export class ToDo {
+    type: string;
+    item: Number;
+    displayName: string;
+    location: any;
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,32 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import { ToDoService } from '../../services/todo.service';
+import {ToDo} from "../../components/todo";
+
+
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers:[ToDoService]
 })
 export class HomePage {
+  todos: ToDo[];
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private ToDoService: ToDoService) {
+    //getToDos(): void {
+      this.todos = this.ToDoService.getToDos();
+    console.log(this.todos);
+    this.ToDoService.addToDo({
+      type: "get",
+      item: 16,
+      displayName: 'Nice',
+      location: {lat: 56, lng: 45, placeName: "Stop and Shop", address: "123 Common Ave", placeID: 34555}
+    });
+    console.log('secondar');
+    console.log(this.todos);
+    //}
   }
 
 }
